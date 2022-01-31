4d1d23a5fac6cb04aba0ba81d2b007eee925db03
ArHiFranki <franki550@gmail.com>
2022-01-31 16:48:01 +0700

    Переименовал основную сцену, добавил перезапуск сцены при столкновении игрока с чем-либо

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(PlayerControls))]
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collides with: " + collision.gameObject.name);
-    }
+    [SerializeField] private float loadDelay = 1f;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+        StartCrashQequence();
+    }
+
+    private void StartCrashQequence()
+    {
+        GetComponent<PlayerControls>().enabled = false;
+        StartCoroutine(RestartLevelSequence(loadDelay));
+    }
+
+    private IEnumerator RestartLevelSequence(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
