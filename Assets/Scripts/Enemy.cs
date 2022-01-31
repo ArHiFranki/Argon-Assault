fcbb038d3a271e4a392123c22dbfea2be874c8c7
ArHiFranki <franki550@gmail.com>
2022-01-31 23:05:14 +0700

    Добавил систему набора очков

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Score per kill")]
+    [SerializeField] private int scorePerHit;
     [SerializeField] private GameObject deathVFX;
     [SerializeField] private Transform parent;
 
+    private ScoreBoard myScoreBoard;
+
+    private void Awake()
+    {
+        myScoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        DestroyEnemy();
+    }
+
+    private void ProcessHit()
+    {
+        myScoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    private void DestroyEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
