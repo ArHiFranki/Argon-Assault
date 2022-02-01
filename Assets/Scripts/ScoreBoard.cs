903aaacc1b02e6b56b1e6eca0d158201833580a1
ArHiFranki <franki550@gmail.com>
2022-02-01 10:58:16 +0700

    Добавил отображение очков в UI

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     private int score;
+    private TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        DisplayScore();
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
-        Debug.Log($"Score: {score}");
+        DisplayScore();
+    }
+
+    private void DisplayScore()
+    {
+        scoreText.text = score.ToString();
     }
 }
