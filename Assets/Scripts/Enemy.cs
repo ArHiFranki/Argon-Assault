4d87b5e7b28bc4a5cc70f233cfd83ce9242c85ca
ArHiFranki <franki550@gmail.com>
2022-02-02 10:58:05 +0700

    Добавил возможность настройки противникам количества HP и визуальный эффект при попадании

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [Header("Score per kill")]
+    [Header("General Settings")]
+    [SerializeField] private int enemyHitPoints;
     [SerializeField] private int scorePerHit;
+
+    [Header("Particle Systems Effects")]
     [SerializeField] private GameObject deathVFX;
+    [SerializeField] private GameObject hitVFX;
     [SerializeField] private Transform parent;
 
     private ScoreBoard myScoreBoard;
@@ -19,18 +23,33 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        DestroyEnemy();
     }
 
     private void ProcessHit()
     {
         myScoreBoard.IncreaseScore(scorePerHit);
+        InstantiateVFX(hitVFX);
+        ReduseEnemyHitPoints();
+    }
+
+    private void InstantiateVFX(GameObject particleVFX)
+    {
+        GameObject vfx = Instantiate(particleVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+    }
+
+    private void ReduseEnemyHitPoints()
+    {
+        enemyHitPoints--;
+        if (enemyHitPoints < 1)
+        {
+            DestroyEnemy();
+        }
     }
 
     private void DestroyEnemy()
     {
-        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        InstantiateVFX(deathVFX);
         gameObject.SetActive(false);
         Destroy(gameObject);
     }
